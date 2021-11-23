48e3d89930639504462383916ff8f24b0c3db633
GH-LiuYu <979036781@qq.com>
2021-11-23 17:37:43 +0800

    '20211123'

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,55 @@
+module.exports = {
+    theme: 'reco',
+    title: 'vuepress-theme-reco',
+    description: '一款简洁而优雅的 vuepress 博客 & 文档 主题',
+
+    themeConfig: {
+        sidebar: 'auto',
+        style: '@vuepress-reco/style-default',
+        logo: '/logo.png',
+        author: 'reco_luan',
+        docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
+        docsBranch: 'main',
+        docsDir: 'example',
+        lastUpdatedText: '',
+        // series 为原 sidebar
+        series: {
+            '/docs/theme-reco/': [
+                {
+                    text: 'module one',
+                    children: ['home', 'theme']
+                },
+                {
+                    text: 'module two',
+                    children: ['api', 'plugin']
+                }
+            ]
+        },
+        nav:
+            [
+                { text: '文档', link: '/',icon: 'reco-document'},
+                { text: '博客', link: '/blog/index' ,icon: 'reco-blog'},
+                { text: '标签', link: '/tags/tag1/1/',icon: 'reco-tag' },
+                { text: 'GitHub', link: 'https://github.com/GH-LiuYu',icon: 'reco-github' },
+                { text: '案例',
+                    items: [
+                        { text: '下拉1', link: '/docs/theme-reco/theme' },
+                        { text: '下拉2', link: '/blogs/other/guide' }
+                    ]
+                },
+            ],
+    },
+    plugins: [
+        [
+            'vuepress-plugin-sponsor',
+            {
+                theme: 'simple',
+                alipay: '/logo.png',
+                wechat: '/logo.png',
+                qq: '/logo.png',
+                paypal: 'https://www.paypal.me/yokefellow',
+                duration: 2000
+            }
+        ]
+    ]
+}
---
+++ docs/README.md
@@ -0,0 +1,54 @@
+---
+home: true
+showSponsor: true
+heroImage: /logo.png
+heroImageStyle: {
+  maxWidth: '200px',
+  width: '100%',
+  display: block,
+  margin: '0 auto 2rem',
+  borderRadius: '1rem',
+}
+heroText: vuepress-theme-reco-next
+tagline: 一款简洁而优雅的 vuepress 博客 & 文档 主题。
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+# vuepress-theme-reco-demo
+
+Blog demo for vuepress-theme-reco
+
+## Experience
+
+**npx**
+
+```
+npx @vuepress-reco/theme-cli init
+```
+
+**npm**
+
+```bash
+# 初始化
+npm install @vuepress-reco/theme-cli -g
+theme-cli init
+```
+
+**yarn**
+
+```bash
+# 初始化
+yarn global add @vuepress-reco/theme-cli
+theme-cli init
+```
+
